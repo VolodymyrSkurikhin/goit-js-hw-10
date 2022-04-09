cf091a3a25b6c515b3c2f123745c5bd584f3ef75
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-04-09 18:23:05 +0300

    initial

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export function fetchCountries(name) {
+  return fetch('https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages')
+    .then(response => {
+      if (!response.ok) {
+      throw new Error(response.status)
+      }
+      return response.json()
+    })
+    .then(console.log)
+};
---
+++ src/src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import fetchCountries from ('fetchCountries');
+
+const DEBOUNCE_DELAY = 300;
+
+
+fetchCountries(India);
